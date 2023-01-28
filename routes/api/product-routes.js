fcf40760e95b725e6f01a5325c7d82910274dcfc
Tyler Walton <tylerwalton300@gmail.com>
2023-01-27 20:11:27 -0500

    finished all routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,7 +17,7 @@ router.get("/:id", (req, res) => {
   // be sure to include its associated Category and Tag data
   Product.findOne({
     where: { id: req.params.id },
-    include: [{ model: Category, attributes: ['category_name'] }] , [{model : Tag, attributes : ['tag_name']}]
+    include: [{ model: Category, attributes: ['category_name'] } , {model : Tag, attributes : ['tag_name']}]
   })
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json(err));
@@ -99,7 +99,7 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
-  Product.delete(req.body, {where : {id : id.params.id}})
+  Product.destroy({where : {id : req.params.id}})
    .then ((data) => res.status(200) .json(data))
    .catch ((err) => res. status(400).json (err));
 });
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,9 +6,9 @@ const { Tag, Product, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  Tag.findAll({ include: [{model : Product}]
+  Tag.findAll({ include: [{model : Product}] })
     .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(400).json(err));
+    .catch((err) => res.status(400).json(err))
 });
 
 router.get("/:id", (req, res) => {
@@ -35,9 +35,9 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
-  Tag.delete(req.body, {where : {id: id.params.id}})
+  Tag.destroy({where : {id: req.params.id}})
     .then((data) => res.status(200).json(data))
     .catch((err) => res.status(400).json(err));
 });
 
-module.exports = router;
+module.exports = router
